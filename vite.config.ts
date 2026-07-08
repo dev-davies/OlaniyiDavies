65ca4b6c960f19df71b38b0ff706fbd9afe01383
novelacadhost-sketch <dvsolaniyi@gmail.com>
2026-07-08 13:46:09 +0100

    feat: integrate unplugin-vue-markdown and prism for markdown rendering in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,23 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Markdown from 'unplugin-vue-markdown/vite'
+import Prism from 'markdown-it-prism'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue()],
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/], // allow Vue to process .md files
+    }),
+    Markdown({
+      wrapperClasses: 'prose',
+      markdownItSetup(md) {
+        md.use(Prism)
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
